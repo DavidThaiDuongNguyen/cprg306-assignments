f64ecadfbe2d328f54f3e10eafc9cc6ff20391b3
DavidThaiDuongNguyen <Thai.Nguyen1@edu.sait.ca>
2025-09-18 16:19:35 -0600

    Added Week 2 folders and components inside the app folder. Also edited and changed the app/page.js file

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Thai Duong (David) Nguyen </p>
+      <p>
+        GitHub Repository:
+        <Link href="https://github.com/DavidThaiDuongNguyen/cprg306-assignments">
+          DavidThaiDuongNguyen/cprg306-assignments
+        </Link>
+      </p>
+    </div>
+  );
+}
