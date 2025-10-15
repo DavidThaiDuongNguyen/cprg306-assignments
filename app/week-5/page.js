5347f247a2b9f6cceca619f0cec7f9094fe9c812
DavidThaiDuongNguyen <Thai.Nguyen1@edu.sait.ca>
2025-10-14 20:15:44 -0600

    week 5 page added

---
+++ app/week-5/page.js
@@ -0,0 +1,10 @@
+import NewItem from "./new-item";
+
+export default function Page() {
+  return (
+    <main>
+      < h1 className="text-2xl text-center my-5 font-bold" >Week 5 - New Item</h1 >
+      <NewItem />
+    </main >
+  );
+}
